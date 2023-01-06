b3e9e5bad99ad71948fd09be889b0084d0118173
barisskann <bars_958@hotmail.com>
2023-01-06 15:29:48 +0300

    REACT APP1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./app.css";
+import { useEffect, useState } from "react";
+import data from "./data";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+  const [click, setClick] = useState(0);
+  const [total, setTotal] = useState(0);
+  const [dizi, setDizi] = useState([]);
+  const [veri, setVeri] = useState(0);
+  const handleclick = (item) => {
+    setClick(click + 1);
+    setTotal(total + item.id);
+    setDizi([...dizi, item]);
+  };
+  console.log(data);
+  const handleRandom = () => {
+    setVeri(veri + 1);
+  };
+  if (click === 2) {
+    if (total === 10) {
+      dizi[0].disabled = true;
+      dizi[1].disabled = true;
+    }
+    setClick(0);
+    setTotal(0);
+    setDizi([]);
+  }
+  useEffect(() => {
+    data.sort(() => 0.5 - Math.random());
+  }, [veri]);
+  let render = data.map((item) => {
+    return (
+      <span key={item.id}>
+        <button
+          disabled={item.disabled}
+          onClick={() => {
+            handleclick(item);
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          {item.id}
+        </button>
+      </span>
+    );
+  });
+
+  return (
+    <div>
+      <div>{render}</div>
+      <div>{click}</div>
+      <div>{total}</div>
+      <button onClick={handleRandom}>wqeqsw</button>
     </div>
   );
 }
-
 export default App;
---
+++ src/data.js
@@ -0,0 +1,40 @@
+let data = [
+  {
+    id: 1,
+    disabled: false,
+  },
+  {
+    id: 2,
+    disabled: false,
+  },
+  {
+    id: 3,
+    disabled: false,
+  },
+  {
+    id: 4,
+    disabled: false,
+  },
+  {
+    id: 5,
+    disabled: false,
+  },
+  {
+    id: 6,
+    disabled: false,
+  },
+  {
+    id: 7,
+    disabled: false,
+  },
+  {
+    id: 8,
+    disabled: false,
+  },
+  {
+    id: 9,
+    disabled: false,
+  },
+];
+
+export default data;
